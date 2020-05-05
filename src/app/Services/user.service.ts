983554d55f396e565c164618d587a30e52e58a56
Norelli-Christian <christian.norelli@gmail.com>
2020-05-05 19:29:17 +0200

    create components

--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/User';
+import { LocalStorageService } from 'ngx-webstorage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  loggedUser:User;
+  constructor(public localStorage:LocalStorageService) { }
 
-  constructor() { }
+  login(){
+    this.loggedUser={
+      id:1,
+      firstname:"Christian",
+      lastname:"Norelli",
+      favoritesFilm:[]
+    }
+  }
+  save(){
+    this.localStorage.store('users', this.loggedUser);
+  }
+
+  logout(){
+    this.loggedUser = null;
+    this.localStorage.clear("loggedUser");
+  }
+
+  getLoggedUser(){
+    this.loggedUser = this.localStorage.retrieve('loggedUser');
+  }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FilmsComponent } from './films/films.component';
+import { AddFilmComponent } from './add-film/add-film.component';
+import { EditFilmComponent } from './edit-film/edit-film.component';
+import { ActorsComponent } from './actors/actors.component';
+import { AddActorComponent } from './add-actor/add-actor.component';
+import { EditActorComponent } from './edit-actor/edit-actor.component';
+import { GenresComponent } from './genres/genres.component';
+import { AddGenreComponent } from './add-genre/add-genre.component';
+import { EditGenreComponent } from './edit-genre/edit-genre.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'films', component: FilmsComponent },
+  { path: 'films/add', component: AddFilmComponent },
+  { path: 'films/edit', component: EditFilmComponent },
+  { path: 'actors', component: ActorsComponent },
+  { path: 'actors/add', component: AddActorComponent },
+  { path: 'actors/edit', component: EditActorComponent },
+  { path: 'genre', component: GenresComponent },
+  { path: 'genre/add', component: AddGenreComponent },
+  { path: 'genre/edit', component: EditGenreComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
